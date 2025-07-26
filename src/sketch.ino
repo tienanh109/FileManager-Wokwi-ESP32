a0b89b01cef03d8095bcde906e8a05417fb4f773
Tien Anh <99895433+tienanh109@users.noreply.github.com>
2025-07-26 13:25:26 +0700

    Update sketch.ino

--- src/sketch.ino
+++ src/sketch.ino
@@ -5,8 +5,8 @@
 
 #define SD_CS 5
 
-const char* ssid = "YOUR_SSID"; // if it was wokwi it would be Wokwi-GUEST
-const char* password = "YOUR_PASSWORD"; // leave empty if it was 
+const char* ssid = "Wokwi-GUEST"; // replace with your SSID
+const char* password = ""; // replace wỉth your password
 
 WebServer server(80);
 File uploadFile;
